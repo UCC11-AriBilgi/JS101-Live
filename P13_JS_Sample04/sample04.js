fd980e3ea4d1619c37318fea18fc9a2afb772f93
ukaracivio <umit.karacivi@outlook.com>
2024-03-25 21:00:16 +0300

    Some changes

---
+++ P13_JS_Sample04/sample04.js
@@ -0,0 +1,39 @@
+﻿function toplama() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var toplam = sayi1 + sayi2;
+
+    document.getElementById("sonuc").innerHTML = toplam;
+}
+
+function carpma() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var carpma = sayi1 * sayi2;
+
+    document.getElementById("sonuc").innerHTML = carpma;
+}
+
+function cikarma() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var fark = sayi1 - sayi2;
+
+    document.getElementById("sonuc").innerHTML = fark;
+}
+
+function bolme() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var sonuc = sayi1 / sayi2;
+
+    document.getElementById("sonuc").innerHTML = sonuc;
+}
