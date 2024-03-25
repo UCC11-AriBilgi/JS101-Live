2f6dfb4502e7d52f60dc0eaf85b63755e43d92ed
ukaracivio <umit.karacivi@outlook.com>
2024-03-25 21:23:23 +0300

    Some changes

---
+++ P14_JS_Sample05/sample05.js
@@ -0,0 +1,11 @@
+﻿function degistir() {
+    var element = document.getElementsByClassName("yazi"); // dokumandaki tüm yazi isimli class a sahip olan elementleri öğrendi.
+
+    var index;
+
+    for (index = 0; index < element.length; index++) {
+        /*element[index].innerHTML = "İçerik JS ile değiştirildi";*/
+        element[index].style.color = red;
+        /*element[index].style.fontSize = 20;*/
+    }
+}
