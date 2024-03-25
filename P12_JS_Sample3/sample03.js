567c947db8d8190321b22d5ad95ff7a2e5093aa3
ukaracivio <umit.karacivi@outlook.com>
2024-03-25 20:46:54 +0300

    Some changes

---
+++ P12_JS_Sample3/sample03.js
@@ -0,0 +1,11 @@
+﻿function sayiAl() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var toplam = sayi1 + sayi2;
+    var toplam1 = sayi1 + " " + sayi2;
+
+    document.getElementById("sonuc1").innerHTML = toplam;
+    document.getElementById("sonuc2").innerHTML = toplam1;
+}
