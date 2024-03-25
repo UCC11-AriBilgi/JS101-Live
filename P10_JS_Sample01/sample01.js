a473b1e05e0b415bbd0ad04f8fdab9c7cd83e634
ukaracivio <umit.karacivi@outlook.com>
2024-03-25 20:30:33 +0300

    Some changes

---
+++ P10_JS_Sample01/sample01.js
@@ -0,0 +1,7 @@
+﻿function fonksiyonum() {
+    document.getElementById("demo").innerHTML="Burası JS ile butona basıldıktan sonra değiştirilmiştir.."
+}
+
+function tarih() {
+    document.write(Date());
+}
---
+++ P11_JS_Sample02/sample02.js
@@ -0,0 +1,7 @@
+﻿function kareAl() {
+    var sayi = parseInt(document.getElementById("sayi").value); // ekrandaki texti okuyup sayiya cevirme
+
+    var karesi = sayi * sayi;
+
+    document.getElementById("sonuc").innerHTML = karesi;
+}
