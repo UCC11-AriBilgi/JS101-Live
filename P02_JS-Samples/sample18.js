2b2a3b62f9e32fd3cc0235b249a739d69c52c495
ukaracivio <umit.karacivi@outlook.com>
2024-04-01 20:23:42 +0300

    Some changes

---
+++ P02_JS-Samples/sample18.js
@@ -0,0 +1,19 @@
+﻿function tikla() {
+    document.getElementById("sonuca").innerHTML = "Butona tıklandı...";
+}
+
+function mesajover() {
+    document.getElementById("sonucb").innerHTML = "Butonun üzerindeyim....";
+}
+
+function mesajout() {
+    document.getElementById("sonucb").innerHTML = "Butonun üzerinden çekildim...";
+}
+
+function mesajbasili() {
+    document.getElementById("sonucc").innerHTML = "Butona basıldı...";
+}
+
+function mesajbasilidegil() {
+    document.getElementById("sonucc").innerHTML = "Butona artık basılmıyor...";
+}
---
+++ P02_JS-Samples/sample19.js
@@ -0,0 +1,7 @@
+﻿function mesajover() {
+    document.getElementById("sonuc").innerHTML = "Beyninizi yakmak mı istiyorsunuz?";
+}
+
+function mesajout() {
+    document.getElementById("sonuc").innerHTML = "Ama yakarken bile harita ile yakın...";
+}
