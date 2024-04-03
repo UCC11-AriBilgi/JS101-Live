033b76ac1084668c1b4135aea325df455db2a962
ukaracivio <umit.karacivi@outlook.com>
2024-04-03 19:29:00 +0300

    Some changes

--- P02_JS-Samples/sample21.js
+++ P02_JS-Samples/sample21.js
@@ -1,1 +1,7 @@
-﻿
+﻿"use strict"
+// Yazılan JS kodlarının ,Javascript standartları dışına çıkmasını engeller ve kodun katı kurallar çerçevesi içinde yazılması gerektiğini bildiren bir direktif. Yani JS kurallarının katı bir şekilde uygulanacağını bildirir. Mesela kod içersinde "use strict" kullanıyorsam tanımsız bir şekilde değişgen kullanımına izin vermiyor.
+
+var slayt = document.getElementsByClassName("slayt");
+var slaytsayisi = slayt.length;
+var slaytNo = 0; // slaytların sıra numarası
+var i = 0;
