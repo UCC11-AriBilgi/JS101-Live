c074925a9fead38278a7a3cdc5b061e9fc174f7c
ukaracivio <umit.karacivi@outlook.com>
2024-04-03 21:06:10 +0300

    Some changes

--- P02_JS-Samples/sample21.js
+++ P02_JS-Samples/sample21.js
@@ -2,23 +2,30 @@
 // Yazılan JS kodlarının ,Javascript standartları dışına çıkmasını engeller ve kodun katı kurallar çerçevesi içinde yazılması gerektiğini bildiren bir direktif. Yani JS kurallarının katı bir şekilde uygulanacağını bildirir. Mesela kod içersinde "use strict" kullanıyorsam tanımsız bir şekilde değişgen kullanımına izin vermiyor.
 
 var slayt = document.getElementsByClassName("slayt");
-var slaytsayisi = slayt.length;
+var slaytSayisi = slayt.length;
 var slaytNo = 0; // slaytların sıra numarası
 var i = 0;
 
 slaytgoster(slaytNo);
-
+/**
+ * 
+ * ?? doğru değerleri alamıyorum
+ */
 function slaytgoster(slaytNumarasi) {
 
-    if (slaytNumarasi >= slaytsayisi) {
+    
+    alert("SlaytNumarasi : " + slaytNumarasi);
+    alert("Slayt Sayısı : " + slaytSayisi);
+
+    if (slaytNumarasi >= slaytSayisi) {
         slaytNo = 0;
     }
 
     if (slaytNumarasi < 0) {
-        slaytNo = slaytsayisi - 1;
+        slaytNo = slaytSayisi - 1;
     }
 
-    for (i = 0; i < slaytsayisi; i++) {
+    for (i = 0; i < slaytSayisi; i++) {
         slayt[i].style.display = "none"; /*Görüntüden kaldıyorum*/
     }
 
---
+++ P02_JS-Samples/sample22.js
@@ -0,0 +1,20 @@
+﻿// Sayfa yüklendiğinde menu div 'inin kapalı gelmesini gostermek için
+var menuDurumu = 0;
+
+function menuDegistir() {
+    var menuObj = document.getElementById("menu");
+
+    if (menuDurumu == 0) {
+        // menüm kapalı durumdadır..bunu acmak lazım
+
+        menuObj.style.display = "block";
+        menuDurumu = 1;
+
+    }
+    else {
+        // menüm acıksa
+        // menüyü kapatalım.
+        menuObj.style.display = "none";
+        menuDurumu = 0;
+    }
+}
